b5f17ab8e2289289fd1d66ace70f1b3563ba2519
MrMepi <91080881+MrMepi@users.noreply.github.com>
2021-09-20 18:55:08 +0200

    Add files via upload

--- src/main/java/com/example/DCBoot/MyListener.java
+++ src/main/java/com/example/DCBoot/MyListener.java
@@ -11,7 +11,8 @@ import java.util.*;
 public class MyListener implements MessageCreateListener {
 
     List<BOSS> listBoss = Arrays.asList(
-            new BOSS("Drzewiec","0:01"),
+            new BOSS("test","00:06"),
+            new BOSS("Drzewiec","01:28"),
             new BOSS("Hugo","05:05"),
             new BOSS("Des","05:05"),
             new BOSS("Tortolla","05:05"),
@@ -19,36 +20,75 @@ public class MyListener implements MessageCreateListener {
             new BOSS("Zwierzobij","04:05"),
             new BOSS("Szczur","02:05"),
             new BOSS("Edward","04:05"),
-            new BOSS("Szaman","03:25"));
+            new BOSS("Szaman","03:25"),
+            new BOSS("BJ","00:10"),
+            new BOSS("M1","02:00"),
+            new BOSS("M2","02:00"),
+            new BOSS("KC","01:57"),
+            new BOSS("MI","02:00"),
+            new BOSS("F","02:00"),
+            new BOSS("W","02:00"),
+            new BOSS("ZS","00:20"),
+            new BOSS("BGW","01:10"),
+            new BOSS("BGK","02:00"),
+            new BOSS("BGT","02:00"),
+            new BOSS("WA","02:00"),
+            new BOSS("DA","02:00"),
+            new BOSS("KT","02:00"),
+            new BOSS("WJ","01:48"),
+            new BOSS("KP","02:00"),
+            new BOSS("Ż","00:20"),
+            new BOSS("ZK","02:00"),
+            new BOSS("KB","02:00")
+            );
 
 
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
-
-        TimerTask task = new TimerTask() {
-            public void run() {
-                if (getText(messageCreateEvent.getMessageContent())[0].equalsIgnoreCase("!boss")) {
-                    messageCreateEvent.getChannel().sendMessage(getText(messageCreateEvent.getMessageContent())[2]);
+        TimerTask task = null;
+        if(getText(messageCreateEvent.getMessageContent())[0].equalsIgnoreCase("!boss")) {
+             task = new TimerTask() {
+                public void run() {
+                    if (getText(messageCreateEvent.getMessageContent())[0].equalsIgnoreCase("!boss")) {
+                        messageCreateEvent.getChannel().sendMessage("@TIMERY "+ getText(messageCreateEvent.getMessageContent())[2]);
+                    }
                 }
-            }
-        };
+            };
+        } else if (getText(messageCreateEvent.getMessageContent())[0].equalsIgnoreCase("!!boss")){
+             task = new TimerTask() {
+                public void run() {
+                    if (getText(messageCreateEvent.getMessageContent())[0].equalsIgnoreCase("!!boss")) {
+                        messageCreateEvent.getChannel().sendMessage("@everyone "+getText(messageCreateEvent.getMessageContent())[2]);
+                    }
+                }
+            };
+        }
         Timer timer = new Timer("Timer");
         String delay = "";
         Date start = null;
         try {
-            if(getText(messageCreateEvent.getMessageContent()).length > 1){
-            delay = listBoss.stream().filter(b -> b.name.equals(getText(messageCreateEvent.getMessageContent())[2])).findFirst().get().time;
-            start = parseDateD(getText(messageCreateEvent.getMessageContent())[1]);
-
+            if(getText(messageCreateEvent.getMessageContent()).length > 1) {
+                delay = listBoss.stream().filter(b -> b.name.equals(getText(messageCreateEvent.getMessageContent())[2])).findFirst().get().time;
+                start = parseDateD(getText(messageCreateEvent.getMessageContent())[1]);
             }
-        } catch (ParseException e) {
-            e.printStackTrace();
+        }catch (ArrayIndexOutOfBoundsException e){
+
         }
-      String[] m = delay.split(":");
-        int minut = Integer.parseInt(m[0])+Integer.parseInt(m[1]);
-      if(start != null) {
-          timer.schedule(task, DateUtils.addMinutes(start, minut).getTime() - start.getTime());
-      }
+        catch (Exception e) {
+
+        }
+        if(!delay.isBlank() && timer != null) {
+            String[] m = delay.split(":");
+            int minut = (Integer.parseInt(m[0]) * 60) + (Integer.parseInt(m[1]) - 5);
+            if (start != null) {
+
+                timer.schedule(task, DateUtils.addMinutes(start, minut).getTime() - start.getTime());
+                if(getText(messageCreateEvent.getMessageContent()).length > 1) {
+                    messageCreateEvent.getChannel().sendMessage(getText(messageCreateEvent.getMessageContent())[2] + " "
+                            + (DateUtils.addMinutes(start, minut).getHours() - 1) + ":" + DateUtils.addMinutes(start, minut).getMinutes());
+                }
+            }
+        }
     }
 
     private static Date parseDateD(String text)
